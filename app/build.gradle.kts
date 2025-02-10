aac24d7f271db483a60959306c8970ac7a9089d5
Hp <ahmedelzanaty001@gmail.com>
2025-02-10 03:27:38 +0200

    Init commit and firebase

---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.ecommerceapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.ecommerceapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
+    dependencies {
+
+        implementation (libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        implementation(libs.coil.compose)
+        implementation(libs.xaccompanist.pager.indicators)
+        implementation(libs.accompanist.pager) // Check for the l implementation ("androidx.Lifecycle:Lifecycle-runtime-ktx:2.x.x")
+        implementation(libs.runtime.livedata)
+        implementation (libs.github.glide)
+        implementation(libs.gson)
+        implementation(libs.androidx.constraintlayout.compose)
+        implementation(libs.androidx.foundation)
+
+    }
+}
+dependencies {
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.firebase.database)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
